50fb43972af9bdd7b5da235cfce8f9c37ef4da58
Chung, Insang <insang@hansung.ac.kr>
2016-08-11 19:13:57 +0900

    initial commit

---
+++ calc_travis/src/main/java/hansung/insang/calc_travis/Calc.java
@@ -0,0 +1,7 @@
+package hansung.insang.calc_travis;
+
+public class Calc {
+	public int add(int x, int y) {
+		return x+y;
+	}
+}
---
+++ calc_travis/src/test/java/hansung/insang/calc_travis/CalcTest.java
@@ -0,0 +1,16 @@
+package hansung.insang.calc_travis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CalcTest {
+
+	@Test
+	public void testAdd() {
+		Calc c = new Calc();
+		assertEquals(30, c.add(10, 20));
+	}
+
+}
